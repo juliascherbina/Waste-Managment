0539237b61459b17eeba0e372119c888588c14a5
Julia Scherbina <juliascherbina15@gmail.com>
2024-01-15 23:27:01 +0200

    make section customer feedback

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Footer } from './Components/Footer/Footer';
 import { Header } from './Components/Header/Header';
 import { Main } from './Components/Main/Main';
 import agency from './images/agency.jpg'
-
+import photo from './images/woman.png'
 
 function App() {
   return <>
@@ -61,7 +61,7 @@ function App() {
               </li>
             </ul>
           </div>
-          <button className='work-btn'> Show more</button>
+          <button className='work-btn'> Show more...</button>
         </div>
       </section>
       <section className='feedback'>
@@ -69,48 +69,49 @@ function App() {
           <h1 className='feedback-header'>customer feedback</h1>
           <div className='feedback-items'>
             <div className='feedback-item'>
-              <img src='' className='feedback-img'></img>
+              <img src={photo} className='feedback-img'></img>
               <h3 className='feedback-name'>July Frojenko</h3>
               <span className='feedback-span'>Sale manager</span>
-              <p className='feedback-text'></p>
+              <p className='feedback-text'>Команда фахівців виявилася професійною та креативною,
+                їхній підхід був індивідуальним та спрямованим на досягнення наших бізнес-цілей.</p>
             </div>
             <div className='feedback-item'>
-              <img src='' className='feedback-img'></img>
+              <img src={photo} className='feedback-img'></img>
               <h3 className='feedback-name'>July Frojenko</h3>
               <span className='feedback-span'>Sale manager</span>
-              <p className='feedback-text'></p>
-            </div>
+              <p className='feedback-text'>Команда фахівців виявилася професійною та креативною,
+                їхній підхід був індивідуальним та спрямованим на досягнення наших бізнес-цілей.</p>            </div>
             <div className='feedback-item'>
-              <img src='' className='feedback-img'></img>
+              <img src={photo} className='feedback-img'></img>
               <h3 className='feedback-name'>July Frojenko</h3>
               <span className='feedback-span'>Sale manager</span>
-              <p className='feedback-text'></p>
-            </div>
+              <p className='feedback-text'>Команда фахівців виявилася професійною та креативною,
+                їхній підхід був індивідуальним та спрямованим на досягнення наших бізнес-цілей.</p>            </div>
             <div className='feedback-item'>
-              <img src='' className='feedback-img'></img>
+              <img src={photo} className='feedback-img'></img>
               <h3 className='feedback-name'>July Frojenko</h3>
               <span className='feedback-span'>Sale manager</span>
-              <p className='feedback-text'></p>
-            </div>
+              <p className='feedback-text'>Команда фахівців виявилася професійною та креативною,
+                їхній підхід був індивідуальним та спрямованим на досягнення наших бізнес-цілей.</p>            </div>
             <button className='left-arrow'>left-arrow</button>
             <button className='right-arrow'>right-arrow</button>
-            <button className='point-btn'>.</button>
+            <button className='point-btn'>. more more more</button>
           </div>
         </div>
       </section>
       <section className='connect'>
-             <h1 className='connect-header'> Connect with us</h1>
-             <form className='connect-form'>
-              <label for='fname'>First name:</label> 
-              <input type='text' id='fname' name='fname'></input>
-              
-              <label for='lname'>Last name:</label> 
-              <input type='text' id='lname' name='lname'></input>
-              
-              <label for='email'>Email</label> 
-              <input type='text' id='email' name='email'></input>
-              <button>Want to connect</button>
-             </form>
+        <h1 className='connect-header'> Connect with us</h1>
+        <form className='connect-form'>
+          <label for='fname'>First name:</label>
+          <input type='text' id='fname' name='fname'></input>
+
+          <label for='lname'>Last name:</label>
+          <input type='text' id='lname' name='lname'></input>
+
+          <label for='email'>Email</label>
+          <input type='text' id='email' name='email'></input>
+          <button>Want to connect</button>
+        </form>
       </section>
     </div>
     <Footer />
