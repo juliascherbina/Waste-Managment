f244af586a1e1fbe2ced6fc000afff075353a2c9
Julia Scherbina <juliascherbina15@gmail.com>
2024-01-16 22:35:27 +0200

    style form

--- src/App.js
+++ src/App.js
@@ -105,13 +105,13 @@ function App() {
         <h1 className='connect-header'> Connect with us</h1>
         <form className='connect-form'>
           <label for='fname'>First name:</label>
-          <input type='text' id='fname' name='fname' value='enter your name'></input>
+          <input type='text' id='fname' name='fname' value='Enter your name'></input>
 
           <label for='lname'>Last name:</label>
-          <input type='text' id='lname' name='lname' value='enter your last name'></input>
+          <input type='text' id='lname' name='lname' value='Enter your last name'></input>
 
           <label for='email'>Email</label>
-          <input type='text' id='email' name='email' value='enter your email' ></input>
+          <input type='text' id='email' name='email' value='Enter your email' ></input>
           <button>Want to connect</button>
         </form>
       </section>
