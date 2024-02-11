33cc719a1a05bca1fbd0bbc9986c54429c87bcab
Julia Scherbina <juliascherbina15@gmail.com>
2024-02-11 23:16:39 +0200

    form

--- src/Components/Connect/Connect.jsx
+++ src/Components/Connect/Connect.jsx
@@ -1,25 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 
 
 
 
-export const Connect=()=>{
-    return <>
-     <section className='connect' id="connect">
-        <h1 className='connect-header'> Connect with us</h1>
-        <form className='connect-form'>
-          <label for='fname'>First name:</label>
-          <input type='text' id='fname' name='fname' value='Enter your name'></input>
+export const Connect = () => {
+  const [form, setForm] = useState('')
+  function saveValue(event) {
+    setForm(event.target.value)
+  }
+  return <>
+    <section className='connect' id="connect">
+      <h1 className='connect-header'> Connect with us</h1>
+      <form className='connect-form'>
+        <label for='fname'>First name:</label>
+        <input type='text' id='fname' name='fname' placeholder='Enter your name' onChange={()=>saveValue}></input>
 
-          <label for='lname'>Last name:</label>
-          <input type='text' id='lname' name='lname' value='Enter your last name'></input>
+        <label for='lname'>Last name:</label>
+        <input type='text' id='lname' name='lname' placeholder="Enter your last name" onChange={e => setForm(e.target.value)}></input>
 
-          <label for='email'>Email</label>
-          <input type='text' id='email' name='email' value='Enter your email' ></input>
-          <button>Want to connect</button>
-        </form>
-      </section>
-    </>
+        <label for='email'>Email</label>
+        <input type='text' id='email' name='email' placeholder='Enter your email' onChange={e => setForm(e.target.value)} ></input>
+        <button>Want to connect</button>
+      </form>
+    </section>
+  </>
 }
